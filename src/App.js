dc1f2ba851ef2204258050009cb25812e076178a
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-26 11:14:50 -0400

    Importando Elementos desde React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
-    <div className="container">
-      Hola Mundo
-    </div>
+    <Container style={{ background: 'lightblue'}}>
+      Hello World
+    </Container>
   );
 }
 
