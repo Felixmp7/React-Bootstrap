75041657c6e6a6a7bfd904cc5b31a575789a053c
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-26 11:50:14 -0400

    Completando primera grilla

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import Grid1 from './components/Grid1'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+// import Grid2 from './components/Grid2'
+// import Grid3 from './components/Grid3'
 
 function App() {
   return (
-    <Container
-      style={{ background: 'lightblue'}}
-      // fluid
-      >
-        <Row>
-          <Col lg={3} style={{background: 'yellow'}}>1 of 2</Col>
-          <Col lg={9} style={{background: 'blue'}}>2 of 2</Col>
-        </Row>
+    <Container>
+        <Grid1 />
+        {/* <Grid2 />
+        <Grid3 /> */}
     </Container>
   );
 }
---
+++ src/components/Grid1.js
@@ -0,0 +1,41 @@
+import React, {Fragment} from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Grid1 = () => {
+  return (
+    <Fragment>
+      <Row style={{background: '#2DB793', color: '#2DB793'}}>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+      </Row>
+      <Row style={{background: '#2DB793', color: '#2DB793'}}>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+      </Row>
+      <Row style={{background: '#2DB793', color: '#2DB793'}}>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+      </Row>
+      <Row style={{background: '#2DB793', color: '#2DB793'}}>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col style={{padding: 30, border: '1px solid white'}}>.</Col>
+      </Row>
+      <Row style={{background: '#2DB793', color: '#2DB793', padding:30}}>.</Row>
+    </Fragment>
+  );
+}
+
+export default Grid1
