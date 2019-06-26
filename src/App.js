24426680de8888ab36e449456ed4f38e6eb8998f
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-26 12:00:16 -0400

    Completando 2da Grilla

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Grid1 from './components/Grid1'
 import Container from 'react-bootstrap/Container'
-// import Grid2 from './components/Grid2'
+import Grid2 from './components/Grid2'
 // import Grid3 from './components/Grid3'
 
 function App() {
   return (
     <Container>
         <Grid1 />
-        {/* <Grid2 />
-        <Grid3 /> */}
+        <Grid2 />
+        {/* <Grid3 /> */}
     </Container>
   );
 }
--- src/components/Grid2.js
+++ src/components/Grid2.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-const Grid2 = (props) => {
+const Grid2 = () => {
   return (
-
+    <Fragment>
+      <Row style={{background: '#FACA71', color: '#FACA71'}}>
+        <Col xs={6} md style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col xs={6} md style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col xs={6} md style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col xs={6} md style={{padding: 30, border: '1px solid white'}}>.</Col>
+      </Row>
+      <Row style={{background: '#FACA71', color: '#FACA71'}}>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+        <Col sm={12} md={6} lg style={{padding: 30, border: '1px solid white'}}>.</Col>
+      </Row>
+    </Fragment>
   );
 }
 
