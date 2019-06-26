ac007e5598d5b6b5d31f4643e237047ffd4c7a1b
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-26 15:00:10 -0400

    Finalizando Header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import PlatziLogo from '../assets/images/platzi-logo.png'
+import './styles/Navbar.css'
 
 const NavbarContainer = () => {
   return (
     <Navbar fixed="top"  bg="dark" expand="lg">
-      <Navbar.Brand>
+      <Navbar.Brand className="text-white">
         <img
           alt="PlatziLogo"
           src={PlatziLogo}
@@ -18,10 +19,10 @@ const NavbarContainer = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Nav>
-          <Nav.Link className="text-white">La conferencia</Nav.Link>
-          <Nav.Link className="text-white">Los oradores</Nav.Link>
-          <Nav.Link className="text-white">El lugar y la fecha</Nav.Link>
-          <Nav.Link className="text-white">Conviertete en orador</Nav.Link>
+          <Nav.Link className="text-primary">La conferencia</Nav.Link>
+          <Nav.Link className="text-primary">Los oradores</Nav.Link>
+          <Nav.Link className="text-primary">El lugar y la fecha</Nav.Link>
+          <Nav.Link className="text-primary">Conviertete en orador</Nav.Link>
           <Nav.Link className="text-success">Comprar Tickets</Nav.Link>
         </Nav>
       </Navbar.Collapse>
