1e36704bb31fc60fc7e56ea91f9b32e12801081f
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-26 14:45:21 -0400

    Utilizando Navbar Collapsable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import PlatziLogo from '../assets/images/platzi-logo.png'
 
 const NavbarContainer = () => {
   return (
-    <Navbar fixed="top" bg="dark" variant="dark">
+    <Navbar fixed="top"  bg="dark" expand="lg">
       <Navbar.Brand>
         <img
           alt="PlatziLogo"
@@ -15,12 +15,15 @@ const NavbarContainer = () => {
         />
         {' Conf Hawai'}
       </Navbar.Brand>
-      <Navbar.Collapse className="justify-content-end">
-        <Nav.Link className="text-white">La conferencia</Nav.Link>
-        <Nav.Link className="text-white">Los oradores</Nav.Link>
-        <Nav.Link className="text-white">El lugar y la fecha</Nav.Link>
-        <Nav.Link className="text-white">Conviertete en orador</Nav.Link>
-        <Nav.Link className="text-success">Comprar Tickets</Nav.Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          <Nav.Link className="text-white">La conferencia</Nav.Link>
+          <Nav.Link className="text-white">Los oradores</Nav.Link>
+          <Nav.Link className="text-white">El lugar y la fecha</Nav.Link>
+          <Nav.Link className="text-white">Conviertete en orador</Nav.Link>
+          <Nav.Link className="text-success">Comprar Tickets</Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
