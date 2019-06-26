ecdec0ddd8a349f14c1f18e908ffb81c55077293
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-26 12:32:17 -0400

    Completando Grid3

--- src/components/Grid3.js
+++ src/components/Grid3.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-const Grid3 = (props) => {
+const Grid3 = () => {
   return (
-
+    <Fragment>
+      <Row>
+        <Col
+          style={{
+            background: 'red',
+            padding: 30,
+            color: 'red',
+            border: '1px solid white'
+          }}
+          lg={{span:6,offset:6}}
+          xs
+          >
+            .
+          </Col>
+      </Row>
+      <Row>
+        <Col
+          style={{
+            background: 'red',
+            padding: 30,
+            color: 'red',
+            border: '1px solid white'
+          }}
+          lg={{span:8,offset:2}}
+          xs={8}
+          >
+            .
+          </Col>
+      </Row>
+      <Row>
+        <Col
+          style={{
+            background: 'red',
+            padding: 30,
+            color: 'red',
+            border: '1px solid white'
+          }}
+          lg={{span:2, offset:1}}
+          xs={{span:2, offset:10}}
+          >
+            .
+          </Col>
+      </Row>
+    </Fragment>
   );
 }
 
